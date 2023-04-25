359de7d5066a2ba613231c3b9cac78dc16bee5ab
Pancu-Robert <robert_george.pancu@stud.acs.upb.ro>
2023-04-26 02:21:59 +0300

    product_detail

--- online_shop/lib/screens/product_detail.dart
+++ online_shop/lib/screens/product_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../providers/product.dart';
 
 import '../widgets/home_app_bar.dart';
 
@@ -9,13 +10,76 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final product = ModalRoute.of(context)!.settings.arguments as Product;
+
     return Scaffold(
       appBar: HomeAppBar(),
-      body: Center(
+      body: SafeArea(
         child: Container(
-          child: Text("Test"),
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Product image
+              Expanded(
+                flex: 2,
+                child: Image.network(
+                  product.imageUrl,
+                  height: double.infinity,
+                ),
+              ),
+              // Product title, description, and price
+              Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Product title
+                      Text(
+                        product.title,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            ?.copyWith(fontSize: 24),
+                      ),
+                      SizedBox(height: 16),
+                      // Product description
+                      Text(
+                        product.description,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            ?.copyWith(fontSize: 16),
+                      ),
+                      SizedBox(height: 16),
+                      // Product price
+                      Text(
+                        product.price.toString(),
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline5
+                            ?.copyWith(fontSize: 36),
+                      ),
+                      SizedBox(height: 16),
+                      // Add to cart button
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('Add to Cart',
+                            style: Theme.of(context)
+                                .textTheme
+                                .button
+                                ?.copyWith(fontSize: 20)),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+     ),
     );
   }
 }
